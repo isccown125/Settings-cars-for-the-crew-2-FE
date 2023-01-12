200df671685c48597298869ad1d70814f8ff1a83
isccown125 <92261514+isccown125@users.noreply.github.com>
2023-01-12 16:15:41 +0100

    refactor home.tsx resolve problem

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import Header from "../components/UI/Header/Header";
 import Navigation from "../components/UI/Navigation/Navigation";
 import MainContent from "../components/UI/WebContentWrapper/MainContent";
 import Footer from "../components/UI/Footer/Footer";
+import Banner from "../components/UI/Banner/Banner";
 import { VehicleReq } from "types";
 
 function Home() {
@@ -16,7 +17,8 @@ function Home() {
     return (
         <>
             <Header>
-                <Navigation></Navigation>
+                <Navigation />
+                <Banner />
             </Header>
             <MainContent>
 
