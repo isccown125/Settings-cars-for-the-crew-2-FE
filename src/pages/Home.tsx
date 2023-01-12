cddc4bf1252d0ff3c8f08ebff2e4f0f44264ab32
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2023-01-12 14:56:25 +0100

    connect backend types with frontend

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,16 @@ import Header from "../components/UI/Header/Header";
 import Navigation from "../components/UI/Navigation/Navigation";
 import MainContent from "../components/UI/WebContentWrapper/MainContent";
 import Footer from "../components/UI/Footer/Footer";
+import { VehicleReq } from "types";
 
 function Home() {
+    const foobar: VehicleReq = {
+        brand: 'xxx',
+        model: 'xxx',
+        period: "xxx",
+        cost: 12,
+        discipline: "xxx",
+    }
     return (
         <>
             <Header>
@@ -14,6 +22,7 @@ function Home() {
 
             </MainContent>
             <Footer>
+                <h1>{foobar.brand}</h1>
             </Footer>
         </>
 
